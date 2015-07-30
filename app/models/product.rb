ebb009adbb5cc34ce8ba52c03b55d7f865690be2
bkroggel <b.kroggel@yahoo.de>
2015-07-30 18:15:14 +0200

    added validation to product (name of product) and comments (all)

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,13 @@ class Product < ActiveRecord::Base
   has_many :orders
   has_many :comments
 
+  # validation
+  validates :name, presence: true
+  #validates :body, presence: true
+  #validates :user, presence: true
+  #validates :product, presence: true
+  #validates :rating, numericality: {only_integer: true}
+
   def average_rating
     comments.average(:rating).to_f
   end
