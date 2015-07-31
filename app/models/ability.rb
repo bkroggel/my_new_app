9d907c59df3af6766cf81f7fa4123d28d2693a66
bkroggel <b.kroggel@yahoo.de>
2015-07-31 12:25:58 +0200

    added restrictet access via cancan module (just admins destroy comments)

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,14 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+
+    if user.admin?
+      can :destroy, Comment
+    else
+      can :read, Comment
+    end
   end
+  
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
