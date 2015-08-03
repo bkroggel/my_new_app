7cfceeb99b9843617c2d5f3e32f84a73500393df
bkroggel <b.kroggel@yahoo.de>
2015-08-03 09:00:17 +0200

    fixed users_path bug in user show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,8 @@ class ApplicationController < ActionController::Base
     redirect_to main_app.root_url, :alert => exception.message
   end
 
-  #added to use username at devise signup
+  # added to use username at devise signup
+  # https://github.com/plataformatec/devise#strong-parameters
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:last_name, :first_name, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :last_name, :first_name, :email, :password, :remember_me) }
