2b30af0dde7a7fa1d6eae7ac1d612ed9f97b53ed
bkroggel <b.kroggel@yahoo.de>
2015-08-10 14:12:04 +0200

    improve DRYness

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
   describe Comment do
     
     context "presence of body" do
+      before { 
+        @user = User.new();
+        @product = Product.new()
+      }
         it "should be invalid without the needed stuff" do
           comment = Comment.new(body: nil, user: nil, product: nil, rating: "blub")
           comment.valid?
@@ -9,17 +13,13 @@ require 'rails_helper'
         end
 
         it "should be valid (everything correctly included)" do
-          user = User.new()
-          product = Product.new()
-          comment = Comment.new(body: "Testcontent", rating: 5, user: user, product: product)
+          comment = Comment.new(body: "Testcontent", rating: 5, user: @user, product: @product)
           comment.valid?
           expect(comment).to be_valid
         end
 
         it "should be invalid because rating is a string" do
-          user = User.new()
-          product = Product.new()
-          comment = Comment.new(body: "Testcontent", rating: "Test", user: user, product: product) 
+          comment = Comment.new(body: "Testcontent", rating: "Test", user: @user, product: @product) 
           comment.valid?
           expect(comment).to be_invalid
         end
@@ -30,20 +30,16 @@ require 'rails_helper'
         # now a string?
         #
         it "it still works although rating is a string and should be integer" do
-          user = User.new()
-          product = Product.new()
-          comment = Comment.new(body: "Testcontent", rating: "5", user: user, product: product) 
+          comment = Comment.new(body: "Testcontent", rating: "5", user: @user, product: @product) 
           comment.valid?
           expect(comment).to be_valid
-        endc
+        end
         #
         #
         #
 
         it "should fail becaus body is empty" do
-          user = User.new()
-          product = Product.new()
-          comment = Comment.new(body: "", rating: 5, user: user, product: product) 
+          comment = Comment.new(body: "", rating: 5, user: @user, product: @product) 
           comment.valid?
           expect(comment).to be_invalid
         end
