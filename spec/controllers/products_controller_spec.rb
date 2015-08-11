79ff1b5a3bd02bc38ae45ab3990ba8692a553977
bkroggel <b.kroggel@yahoo.de>
2015-08-11 13:44:04 +0200

    DRY it

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -3,14 +3,12 @@ require 'rails_helper'
 describe ProductsController, :type => :controller do
   before do
     @user = User.create(email: "max@mustermann.de", password: "12345678")
+    sign_in @user 
+    # get :index, id: @user.id 
   end 
 
   describe "GET #index" do
-    before do 
-    sign_in @user 
-    # get :index, id: @user.id 
-    end 
-    
+   
     it "respond successfully with an HTTP 200 status code" do  
       get :index
       expect(response).to be_success
