d74ba63e91c4093c096018a3d116a194aeedd19d
bkroggel <b.kroggel@yahoo.de>
2015-08-18 14:52:08 +0200

    added rspec test

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -4,7 +4,8 @@ describe ProductsController, :type => :controller do
   before do
     @user = User.create(email: "max@mustermann.de", password: "12345678")
     sign_in @user 
-    # get :index, id: @user.id 
+    get :index, id: @user.id
+    @product = Product.create(name: "Monster", description: "Nice", colour: "Blue", price: "300")
   end 
 
   describe "GET #index" do
@@ -18,6 +19,20 @@ describe ProductsController, :type => :controller do
     it "renders the index template" do
       get :index
       expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET #show" do
+   
+    it "respond successfully with an HTTP 200 status code" do  
+      get :show, id: @product.id
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the show  template" do
+      get :show, id: @product.id
+      expect(response).to render_template("show")
     end
   end
 
