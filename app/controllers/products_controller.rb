6bdc8392971e73c05c1724f2dee50d22e434dac1
bkroggel <b.kroggel@yahoo.de>
2015-08-26 20:04:10 +0200

    back to LIKE

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
     @products = Product.all
     if params[:q]
       search_term = params[:q]
-      @products = Product.where('name ilike ?', "%#{search_term}%")
+      @products = Product.where('name LIKE ?', "%#{search_term}%")
       #return filtered list here
     else
       @products = Product.all
